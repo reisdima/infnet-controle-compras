e6c86dffa778d2f9d23a37da511ae6e4d10c1e90
Caio Vincenzo Reis Dima <cvincenzo@ciandt.com>
2025-08-30 19:16:28 -0300

    Adiciona outros método na controller

--- src/main/java/br/edu/infnet/caiovincenzo/controller/ProdutoController.java
+++ src/main/java/br/edu/infnet/caiovincenzo/controller/ProdutoController.java
@@ -2,9 +2,7 @@ package br.edu.infnet.caiovincenzo.controller;
 
 import br.edu.infnet.caiovincenzo.model.domain.Produto;
 import br.edu.infnet.caiovincenzo.model.service.ProdutoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,4 +20,25 @@ public class ProdutoController {
     public List<Produto> obterLista() {
         return produtoService.obterLista();
     }
+
+    @GetMapping("/{id}")
+    public Produto obterPorId(@PathVariable Integer id) {
+        return produtoService.obterPorId(id);
+    }
+
+    @DeleteMapping (value = "/{id}")
+    public void excluir(@PathVariable Integer id) {
+        produtoService.excluir(id);
+    }
+
+    @PostMapping
+    public Produto incluir(@RequestBody Produto produto) {
+        return produtoService.incluir(produto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public Produto alterar(@PathVariable Integer id, @RequestBody Produto produto) {
+        return produtoService.alterar(id, produto);
+    }
+
 }
