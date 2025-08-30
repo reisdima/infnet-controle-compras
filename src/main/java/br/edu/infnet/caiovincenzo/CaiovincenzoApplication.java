7ee61fa5e530e2a636e016e94ccb1cb300b0ee47
Caio Vincenzo Reis Dima <cvincenzo@ciandt.com>
2025-08-30 17:53:41 -0300

    First commit

---
+++ src/main/java/br/edu/infnet/caiovincenzo/CaiovincenzoApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.caiovincenzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaiovincenzoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaiovincenzoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/caiovincenzo/CaiovincenzoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.caiovincenzo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaiovincenzoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
